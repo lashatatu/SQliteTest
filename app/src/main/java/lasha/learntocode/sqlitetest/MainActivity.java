e332e7217dffe43813f4f28bff34a4e4f4d36ada
lashatatu <lashatatu@gmail.com>
2020-07-01 12:30:52 +0400

    1

---
+++ app/src/main/java/lasha/learntocode/sqlitetest/MainActivity.java
@@ -0,0 +1,68 @@
+package lasha.learntocode.sqlitetest;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        SQLiteDatabase sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqLite-test-1.db", MODE_PRIVATE, null);
+        sqLiteDatabase.execSQL("CREATE TABLE contacts(name TEXT, phone INTEGET, email TEXT);");
+        sqLiteDatabase.execSQL("INSERT INTO contacts VALUES('lasha', 6456789, 'lasha@mail.com');");
+        sqLiteDatabase.execSQL("INSERT INTO contacts VALUES('fred', 12345, 'fred@mail.com');");
+
+        Cursor query = sqLiteDatabase.rawQuery("SELECT * FROM contacts;", null);
+        if (query.moveToFirst()) {
+            String name=query.getString(0);
+            int phone=query.getInt(1);
+            String email=query.getString(2);
+        }
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
